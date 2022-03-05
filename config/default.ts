948cd9b6a54ebfcb87f2b3bdcc0d46d4982487a0
Divinewill ZERO <codewithdiv@users.noreply.github.com>
2022-03-05 22:11:29 +0100

    [Chore ⚙] added new env variables to config

--- config/default.ts
+++ config/default.ts
@@ -8,4 +8,10 @@ export default {
   env: ENV,
   port: process.env.PORT,
   tokenExpiration: '30min',
+  name: process.env.APP_NAME,
+  from: process.env.MAIL_FROM,
+  MAIL_HOST: process.env.MAIL_HOST,
+  MAIL_PASSWORD: process.env.MAIL_PASSWORD,
+  MAIL_USER: process.env.MAIL_USER,
+  MAIL_PORT: process.env.MAIL_PORT,
 };
